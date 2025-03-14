896fbca5de9e8116934e140c3240d31cce4c906b
snnktri <skktri237@gmail.com>
2025-03-14 08:04:13 +0545

    image api handler added

---
+++ client/src/apiHandler/imageApi.js
@@ -0,0 +1,17 @@
+import { api } from "../utils/axiosInstance";
+
+export const imageUploader = async (image) => {
+    try {
+        console.log(image);
+        console.log("jello");
+        const response = await api.post('/images/uploadImage', image,{
+                headers: {
+                  "Content-Type": "multipart/form-data",
+                },
+            }
+        )
+        return response.data;
+    } catch (error) {
+        console.error("Error uploading image. ", error.message);
+    }
+}
