57a42cadc3bda3f6dc3313e3394597e1b3a889c1
snnktri <skktri237@gmail.com>
2025-04-28 09:14:18 +0545

    blog route updated with blog by id

--- server/src/routes/blog.route.js
+++ server/src/routes/blog.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createBlog, deleteBlog, updateBlog, getAllBlogs, getAllBlogsOfAllUsers } from "../controllers/blog.controller.js";
+import { createBlog, deleteBlog, updateBlog, getAllBlogs, getAllBlogsOfAllUsers, getBlogById } from "../controllers/blog.controller.js";
 import { verifyJWT } from "../middelwares/auth.middelware.js";
 
 const router = Router();
@@ -13,5 +13,7 @@ router.route("/deleteBlog/:id").delete(verifyJWT, deleteBlog);
 router.route("/getAllBlogs").get(verifyJWT,getAllBlogs);
 
 router.route("/getAll").get(getAllBlogsOfAllUsers);
+
+router.route("/getById/:id").get(getBlogById);
 
 export default router;
